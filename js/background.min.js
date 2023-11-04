b088ca4ec8e6f5aab85d35b727a2775e9e12375c
屑猫鱼a <zybqw@qq.com>
2023-11-04 12:56:15 +0800

    Site updated: 2023-11-04 12:56:11

---
+++ js/background.min.js
@@ -0,0 +1,5 @@
+<canvas
+    id="background"
+    style="position: fixed; top: 0; left: 0; width: 100vw; height: 100vh; pointer-events: none; z-index: -1"
+></canvas>
+<script src="/js/background.min.js"></script>
---
+++ js/cursor.min.js
@@ -0,0 +1,3 @@
+<div id="cursor"></div>
+<link rel="stylesheet" href="/css/cursor.min.css" />
+<script src="/js/cursor.min.js"></script>
