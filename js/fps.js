54f19752599949c4b4073de0773f908e1e9d643e
Aurzex <zybqw@qq.com>
2024-06-10 10:28:24 +0800

    又又又又更了

---
+++ js/fps.js
@@ -0,0 +1,52 @@
+// if(window.localStorage.getItem("fpson")=="1"){
+//如果要使博客设置上面的设置项能生效，就把上面一行取消注释
+var rAF = (function () {
+	return (
+		window.requestAnimationFrame ||
+		window.webkitRequestAnimationFrame ||
+		function (callback) {
+			window.setTimeout(callback, 1000 / 60);
+		}
+	);
+})();
+var frame = 0;
+var allFrameCount = 0;
+var lastTime = Date.now();
+var lastFameTime = Date.now();
+var loop = function () {
+	var now = Date.now();
+	var fs = now - lastFameTime;
+	var fps = Math.round(1000 / fs);
+	lastFameTime = now;
+	// 不置 0，在动画的开头及结尾记录此值的差值算出 FPS
+	allFrameCount++;
+	frame++;
+	if (now > 1000 + lastTime) {
+		var fps = Math.round((frame * 1000) / (now - lastTime));
+		var kd;
+		if (fps <= 6) {
+			kd = `<span style="color:#bd0000">卡成ppt</span>`;
+		} else if (fps <= 10) {
+			kd = `<span style="color:red">电竞级帧率</span>`;
+		} else if (fps <= 14) {
+			kd = `<span style="color:yellow">难受</span>`;
+		} else if (fps < 24) {
+			kd = `<span style="color:orange">卡</span>`;
+		} else if (fps <= 40) {
+			kd = `<span style="color:green">...</span>`;
+		} else {
+			kd = `<span style="color:#425aef">正常</span>`;
+		}
+		var fpsElement = document.getElementById("fps");
+		if (fpsElement) {
+			fpsElement.innerHTML = `FPS:${fps} ${kd}`;
+		}
+		frame = 0;
+		lastTime = now;
+	}
+	rAF(loop);
+};
+loop();
+// }
+// else{$("#fps").hide()}
+//如果要使博客设置上面的设置项能生效，就把上面两行取消注释
