58798de054f857ef88b28dd028b853b8aad7c72e
zybqw <zybqw@qq.com>
2024-10-01 22:03:22 +0800

    又又又又更了

--- js/loading.js
+++ js/loading.js
@@ -1,16 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-	var loadAnimation = document.getElementById("Loadanimation");
-	if (loadAnimation) {
-		var fadeEffect = setInterval(function () {
-			if (!loadAnimation.style.opacity) {
-				loadAnimation.style.opacity = 1;
-			}
-			if (loadAnimation.style.opacity > 0) {
-				loadAnimation.style.opacity -= 0.05;
-			} else {
-				clearInterval(fadeEffect);
-				loadAnimation.style.display = "none";
-			}
-		}, 25); // 设置每次递减的间隔时间
-	}
-});
+document.addEventListener("DOMContentLoaded",(function(){var t=document.getElementById("Loadanimation");if(t)var e=setInterval((function(){t.style.opacity||(t.style.opacity=1),t.style.opacity>0?t.style.opacity-=.05:(clearInterval(e),t.style.display="none")}),25)}));
